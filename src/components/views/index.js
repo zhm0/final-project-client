69c3f4fb08c6b81d469172b5e64a7797f854847f
Mike Zhang <zhangmike0@gmail.com>
2023-05-16 18:16:57 -0400

    Added NewCampusView to barrel

--- src/components/views/index.js
+++ src/components/views/index.js
@@ -8,6 +8,7 @@ Note: A "barrel" file is a way to rollup exports from other modules into a singl
 The "barrel" (module) file re-exports the exports of other modules.
 ================================================== */
 export { default as AllCampusesView } from "./AllCampusesView";
+export { default as NewCampusView } from "./NewCampusView";
 export { default as AllStudentsView } from "./AllStudentsView";
 export { default as NewStudentView } from "./NewStudentView";
 export { default as CampusView } from "./CampusView";
