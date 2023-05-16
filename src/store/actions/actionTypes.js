5247aa4a5f0ee5015270ab874b8edeaad5954e46
Mike Zhang <zhangmike0@gmail.com>
2023-05-16 18:12:53 -0400

    Added in action type for adding and removing campuses

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -2,6 +2,8 @@
 
 //All campuses
 export const FETCH_ALL_CAMPUSES = "FETCH_ALL_CAMPUSES";
+export const ADD_CAMPUS = "ADD_CAMPUS";
+export const DELETE_CAMPUS = "DELETE_CAMPUS";
 
 //Single campus
 export const FETCH_CAMPUS = "FETCH_CAMPUS";
