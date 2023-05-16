0a877356afd11645a4197d147f0262ce8c503fa8
Mike Zhang <zhangmike0@gmail.com>
2023-05-16 19:40:13 -0400

    Informative message shown to add students if none enrolled

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -4,7 +4,38 @@ CampusView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display a single campus and its students (if any).
 ================================================== */
+import { Fragment } from "react";
 import { Link } from "react-router-dom";
+
+const ListStudents = (props) => {
+  const { students } = props;
+  if (!students.length) {
+    return (
+      <div>
+        <p>There are no students.</p>
+        <Link to={`/newstudent`}>
+          <button>Add New Student</button>
+        </Link>
+      </div>
+      );
+  }
+
+  return (
+  <Fragment>
+    {students.map( student => {
+      let name = student.firstname + " " + student.lastname;
+      return (
+      <div key={student.id}>
+        <Link to={`/student/${student.id}`}>
+          <h3>{name}</h3>
+        </Link>             
+      </div>
+      );
+      })}
+  </Fragment>
+  );
+
+}
 
 // Take in props data to construct the component
 const CampusView = (props) => {
@@ -16,16 +47,8 @@ const CampusView = (props) => {
       <h1>{campus.name}</h1>
       <p>{campus.address}</p>
       <p>{campus.description}</p>
-      {campus.students.map( student => {
-        let name = student.firstname + " " + student.lastname;
-        return (
-          <div key={student.id}>
-            <Link to={`/student/${student.id}`}>
-              <h2>{name}</h2>
-            </Link>             
-          </div>
-        );
-      })}
+      <h2>Students</h2>
+      <ListStudents students={campus.students}/>
     </div>
   );
 };
