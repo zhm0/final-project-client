e97128bb71aa2e8aaaf9793b7f04088f8a3a8203
Mike Zhang <zhangmike0@gmail.com>
2023-05-16 18:15:34 -0400

    Added cases for adding and deleting campuses in reducer

--- src/store/reducers/campuses.js
+++ src/store/reducers/campuses.js
@@ -12,6 +12,10 @@ const allCampuses = (state = [], action) => {  // Empty array as default Initial
   switch (action.type) {
     case at.FETCH_ALL_CAMPUSES:
       return action.payload;
+    case at.ADD_CAMPUS:
+      return [...state, action.payload]
+    case at.DELETE_CAMPUS:
+      return state.filter(campus => campus.id!==action.payload);
     default:
       // If the Reducer doesn't recognize the Action Type, returns the previous (current) State unchanged.
       return state;
