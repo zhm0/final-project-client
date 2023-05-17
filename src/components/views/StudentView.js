2010e3c632ffe02e7526a8db130ac2e82c5a6e08
Mike Zhang <zhangmike0@gmail.com>
2023-05-17 03:43:31 -0400

    Showed more info linked to edit, ability to go to campus view if there is one, and informative message if student not enrolled and delete student functionality

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,14 +4,42 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+
+import { Link } from "react-router-dom";
+
+const CampusCheck = (props) => {
+  const { student } = props;
+  if (student.campusId) {
+    return (
+      <Link to={`/campus/${student.campusId}`}>
+        <h3>{student.campus.name}</h3>
+      </Link>
+    );
+  }
+  
+  return (
+    <h3>Currently not enrolled in college</h3>
+  )
+}
+
 const StudentView = (props) => {
   const { student } = props;
 
   // Render a single Student view 
   return (
     <div>
+      <br/>
+      <img src={student.imageUrl} alt="Broken link to profile picture of student" style={{width: '240px'}}></img>
       <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus.name}</h3>
+      <CampusCheck student={student}/>
+      <Link to={`/campus/${student.id}/editstudent`}>
+        <button>Edit Student</button>
+      </Link>
+      <p>{student.email}</p>
+      <p>{student.gpa}</p>
+      <Link to={'/students'}>
+        <button onClick={() => props.deleteStudent(student.id)}>Delete Student</button>
+      </Link>
     </div>
   );
 
