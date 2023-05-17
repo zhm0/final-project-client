41aa405d0eb960201b540750650beac698deda60
Mike Zhang <zhangmike0@gmail.com>
2023-05-17 02:17:32 -0400

    Added thunk creator for edit campus

--- src/store/thunks.js
+++ src/store/thunks.js
@@ -48,6 +48,19 @@ export const deleteCampusThunk = id => async dispatch => {  // The THUNK
   }
 };
 
+// Edit Campus
+// THUNK CREATOR:
+export const editCampusThunk = campus => async dispatch => {  // The THUNK
+  try {
+    // API "put" call to update campus (based on "id" and "campus" object's data) from database
+    let updatedCampus = await axios.put(`/api/campuses/${campus.id}`, campus);
+    // Update successful so change state with dispatch
+    dispatch(ac.editCampus(updatedCampus));
+  } catch(err) {
+    console.error(err);
+  }
+};
+
 // Single Campus
 // THUNK CREATOR:
 export const fetchCampusThunk = (id) => async (dispatch) => {  // The THUNK
