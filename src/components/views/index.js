bb7d1ab2aee078028302ed9104ebe7881b12b4ea
Mike Zhang <zhangmike0@gmail.com>
2023-05-17 02:19:56 -0400

    Added EditCampusView to barrel

--- src/components/views/index.js
+++ src/components/views/index.js
@@ -9,6 +9,7 @@ The "barrel" (module) file re-exports the exports of other modules.
 ================================================== */
 export { default as AllCampusesView } from "./AllCampusesView";
 export { default as NewCampusView } from "./NewCampusView";
+export { default as EditCampusView } from "./EditCampusView";
 export { default as AllStudentsView } from "./AllStudentsView";
 export { default as NewStudentView } from "./NewStudentView";
 export { default as CampusView } from "./CampusView";
